4f8faa0288e00ba6ec80163a7a423c8d61056c08
HelenCEBM <helencebm@gmail.com>
2022-03-22 11:34:21 +0000

    codelists

--- analysis/codelists.py
+++ analysis/codelists.py
@@ -1,11 +1,61 @@
-from cohortextractor import (
-    codelist,
-    codelist_from_csv,
+﻿######################################
+
+# Some covariates used in the study are created from codelists of clinical conditions or 
+# numerical values available on a patient's records.
+# This script fetches all of the codelists identified in codelists.txt from OpenCodelists.
+
+######################################
+
+
+# --- IMPORT STATEMENTS ---
+
+## Import code building blocks from cohort extractor package
+from cohortextractor import (codelist, codelist_from_csv, combine_codelists)
+
+
+# --- CODELISTS ---
+
+## ELIGIBILITY CRITERIA VARIABLES ----
+
+
+### Require hospitalisation for COVID-19
+covid_icd10_codes = codelist_from_csv(
+  "codelists/opensafely-covid-identification.csv",
+  system = "icd10",
+  column = "icd10_code",
 )
 
-ethnicity_codes = codelist_from_csv(
-    "codelists/opensafely-ethnicity.csv",
-    system="ctv3",
-    column="Code",
-    category_column="Grouping_6",
+
+## CLINICAL/DEMOGRAPHIC COVARIATES ----
+
+### Ethnicity
+ethnicity_primis_snomed_codes = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-eth2001.csv",
+  system = "snomed",
+  column = "code",
+  category_column="grouping_6_id",
 )
+
+
+# OTHER COVARIATES ----
+
+## Care home 
+care_home_primis_snomed_codes = codelist_from_csv(
+    "codelists/primis-covid19-vacc-uptake-longres.csv", 
+    system = "snomed", 
+    column = "code")
+
+
+## Housebound
+housebound_opensafely_snomed_codes = codelist_from_csv(
+    "codelists/opensafely-housebound.csv", 
+    system = "snomed", 
+    column = "code"
+)
+
+no_longer_housebound_opensafely_snomed_codes = codelist_from_csv(
+    "codelists/opensafely-no-longer-housebound.csv", 
+    system = "snomed", 
+    column = "code"
+)
+
