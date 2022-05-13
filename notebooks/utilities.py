54b440142c81c71b8c7718dea9158b95b9930699
HelenCEBM <helencebm@gmail.com>
2022-05-13 20:38:12 +0100

    rounding

--- notebooks/utilities.py
+++ notebooks/utilities.py
@@ -9,14 +9,15 @@ def redact_small_numbers(df, n, rate_column):
     Args:
         df: measures dataframe
         n: threshold for low number suppression
-        numerator: column name for numerator
-        denominator: column name for denominator
         rate_column: column name for rate
     
     Returns:
         Input dataframe with low numbers suppressed
     """
-    
+    # round to nearest five
+    for c in df.columns:
+        df[c] = ((df[c]/5).round(0)*5).astype(int)
+
     def suppress_column(column):   
         suppressed_count = column[column<=n].sum()
         
@@ -25,17 +26,18 @@ def redact_small_numbers(df, n, rate_column):
             pass
         
         else:
-            column = column.replace([0, 1, 2, 3, 4, 5],np.nan)
+            
+            column = column.replace([0, 5], np.nan)
 
             #while suppressed_count <=n:
                 #suppressed_count += column.min()
                 #column.iloc[column.idxmin()] = np.nan   
         return column
     
-    
-    for column in df.columns.drop(rate_column):
-        df[column] = suppress_column(df[column])
-    
-        df.loc[(df[column].isna()), rate_column] = np.nan
+    if rate_column:
+        for column in df.columns.drop(rate_column):
+            df[column] = suppress_column(df[column])
+        
+            df.loc[(df[column].isna()), rate_column] = np.nan
     
     return df    
